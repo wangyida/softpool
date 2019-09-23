d4fe153fddb3f8cbeee7e585af8956538934b2f7
Yida Wang <yidawang.cn@gmail.com>
2019-09-23 17:20:07 +0200

    vis

--- point_vis.py
+++ point_vis.py
@@ -1,17 +1,27 @@
 # examples/Python/Basic/pointcloud.py
 
+import argparse
 import numpy as np
 import open3d as o3d
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description='Parser added')
+    parser.add_argument(
+        '-f',
+        action="store",
+        dest="file_path",
+        default="",
+        help='Destination for storing results')
+    parser.print_help()
+    results = parser.parse_args()
     print("Load a ply point cloud, print it, and render it")
-    pcd = o3d.io.read_point_cloud("/Users/yidawang/Downloads/pcds/00000000_e5c91fa85ebb60073d89c6ed56e66593_fl001_rm0001_0000.ply")
+    pcd = o3d.io.read_point_cloud(results.file_path)
     print(pcd)
     print(np.asarray(pcd.points))
     o3d.visualization.draw_geometries([pcd])
 
     print("Recompute the normal of the downsampled point cloud")
     pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
-        radius=0.1, max_nn=30))
+        radius=0.1, max_nn=100))
     o3d.visualization.draw_geometries([pcd])
