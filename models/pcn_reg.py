3bcb5db3f050f8885bd56f1202045b57a990cc00
wangyida <yidawang.cn@gmail.com>
2020-03-27 11:38:28 +0100

    add plots

--- models/pcn_reg.py
+++ models/pcn_reg.py
@@ -130,7 +130,7 @@ class Model:
         # p_fine_samp = tf.reduce_mean(p_fine_feat, [1])
         entropy = tf.nn.relu(tf.log(self.channels*1.0) + tf.reduce_mean(tf.reduce_sum(p_coar_samp * tf.log(p_coar_samp), [1]), [0]))
         # entropy += tf.nn.relu(tf.log(self.channels*1.0) + tf.reduce_mean(tf.reduce_sum(p_fine_samp * tf.log(p_fine_samp), [1]), [0]))
-        entropy += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=p_can_feat, logits=p_coar_feat))
+        # entropy += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=p_can_feat, logits=p_coar_feat))
         loss_coarse = chamfer(coarse[:,:,0:3], gt[:,:,0:3])
         loss_coarse += chamfer(self.canonical[:,:,0:3], self.gt_can[:,:,0:3])
         """
@@ -161,7 +161,6 @@ class Model:
         """
         add_train_summary('train/fine_loss', loss_fine)
         update_fine = add_valid_summary('valid/fine_loss', loss_fine)
-
         loss = alpha * loss_coarse + loss_fine + entropy
         # loss = loss_coarse + entropy
         add_train_summary('train/loss', loss)
