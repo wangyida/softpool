ab4feddce6f4ed927962cd069d107be7085d6a37
wangyida <yidawang.cn@gmail.com>
2020-11-05 10:54:28 +0100

    correct colors

--- pytorch/train.py
+++ pytorch/train.py
@@ -18,8 +18,8 @@ import emd_module as emd
 sys.path.append("./chamfer/")
 import dist_chamfer as cd
 from extensions.gridding_loss import GriddingLoss
-# gridding_loss = GriddingLoss(scales=[64, 128], alphas=[0.5, 0.1])
-gridding_loss = GriddingLoss(scales=[64], alphas=[0.1])
+gridding_loss = GriddingLoss(scales=[64, 128], alphas=[0.1, 0.1])
+# gridding_loss = GriddingLoss(scales=[128], alphas=[0.1])
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -69,7 +69,7 @@ class FullModel(nn.Module):
         dist, indexes = self.EMD(output1, gt, eps, iters)
         emd1 += torch.sqrt(dist).mean(1)
         grid_loss = gridding_loss(output1, gt)
-        # emd1 += grid_loss
+        emd1 += grid_loss
         # sqrt_mean = torch.mean(torch.sqrt(torch.mean((output1[i]-gt_regions[i])**2, 2)))
         """
         dist, indexes = self.EMD(output1[i][:,:1024,:], gt_regions[i], eps, iters)
