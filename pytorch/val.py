2ce8343c0747b01744e0225e5a97ce08962c8925
wangyida <yidawang.cn@gmail.com>
2020-12-18 13:59:10 +0100

    correct siren

--- pytorch/val.py
+++ pytorch/val.py
@@ -186,7 +186,7 @@ elif opt.dataset == 'shapenet':
             'cnt': 0
         }
     }
-    complete3d_benchmark = False
+    complete3d_benchmark = True
     if complete3d_benchmark == True:
         with open(os.path.join('./data/test_shapenet.list')) as file:
             model_list = [line.strip().replace('/', '/') for line in file]
